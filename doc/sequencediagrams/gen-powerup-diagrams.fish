1d17042a4ad35320a81e152d29870a54e37f974a
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-03-10 23:19:23 -0700

    script to generate power up sequence diagrams when we figure out how they're supposed to work

---
+++ doc/sequencediagrams/gen-powerup-diagrams.fish
@@ -0,0 +1,26 @@
+#! /usr/bin/env fish
+
+set powerups 'expanded paddle power up'
+set -a powerups 'shrink paddle power up'
+set -a powerups 'self invisible paddle power up'
+set -a powerups 'others invisible paddle power up'
+set -a powerups 'invisible ball power up'
+set -a powerups 'shaped paddle curved outwards power up'
+set -a powerups 'shaped paddle curved inwards power up'
+set -a powerups 'shaped paddle bumpy power up'
+set -a powerups 'colour background power up'
+set -a powerups 'self split paddle power up'
+set -a powerups 'others split paddle power up'
+set -a powerups 'add ball power up'
+set -a powerups 'stop aim power up'
+set -a powerups 'bomb power up'
+set -a powerups 'path trace power up'
+
+
+set curr 12
+for x in $powerups
+  echo $x
+  echo "fr$curr.puml"
+  echo ''
+  set curr (math $curr + 1)
+end
