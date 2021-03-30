3177e7a14280612b88b40265fddb237fdb6bd540
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-03-29 20:35:45 -0600

    pls

--- server/server.ts
+++ server/server.ts
@@ -2,10 +2,10 @@ import {
   acceptable,
   acceptWebSocket,
 } from "https://deno.land/std@0.84.0/ws/mod.ts";
-import lobby, {handleSocket} from "./lobby.ts";
+import lobby, { handleSocket } from "./lobby.ts";
 
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-// import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 const port: number = 5000;
@@ -22,8 +22,9 @@ const router = new Router();
 //     origin: "http://localhost:5000"
 //   }),
 // );
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 router.get("/ws", handleSocket);
-await app.listen({port, hostname: "0.0.0.0"})
+await app.listen({ port, hostname: "0.0.0.0" })
