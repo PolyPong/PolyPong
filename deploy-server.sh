2d1b8a32df0463fa5e14cab345c99a8da0119b63
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-04-16 01:31:02 -0600

    deploy faster by not copying as many files

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
 ssh cybera "rm -rf ~/vol/capstone/*"
+rm -rf PolyPong-Common/node_modules
 scp -pr PolyPong-Common cybera:~/vol/capstone/PolyPong-Common
+rm -rf server/coverage
 scp -pr server cybera:~/vol/capstone/server
 scp docker-compose.yml cybera:~/vol/capstone
 scp Dockerfile cybera:~/vol/capstone
